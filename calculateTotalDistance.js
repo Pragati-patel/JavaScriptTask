a3b2c10a44620692b8ffef95392a19c93ba3acd2
Jayesh Patidar <jayesh.patidar@locofast.com>
2022-07-05 21:43:00 +0530

    First challenge to calculcate the distance covered

---
+++ calculateTotalDistance.js
@@ -0,0 +1,35 @@
+/**
+ * The function to calculate distance.
+ */
+function calculateDistance(direction, distance) {
+    
+}
+
+/**
+ * Steps she followed
+ */
+calculateDistance('East', 300)
+calculateDistance('South', 300)
+calculateDistance('East', 450)
+calculateDistance('North', 700)
+calculateDistance('East', 500)
+calculateDistance('North', 50)
+calculateDistance('West', 15)
+
+/**
+ * Bonus 🤩: If you find the way to calculate the total distance then trying modifying the function according to steps below and try to get same output.
+ * 
+ * calculateDistance('East', '300m')
+ * calculateDistance('South', '300m')
+ * calculateDistance('East', '450m')
+ * calculateDistance('North', '700m')
+ * calculateDistance('East', '500m')
+ * calculateDistance('North', '50m')
+ * calculateDistance('West', '15m')
+ */
+
+
+/**
+ * Your task is to modify the function whichever way you want just to log the total distance she traveled.
+ */
+console.log(totalDistanceTraveled)
