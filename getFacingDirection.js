f7b6b96aff35603f2b6145c0b3cb4f3c524fb2fd
--local <--local>
2022-07-06 20:31:14 +0530

    Second challenge to get the facing direction

---
+++ getFacingDirection.js
@@ -0,0 +1,24 @@
+/**
+ * The function which return the direction she is facing to.
+ */
+function getFacingDirection(direction) {
+
+}
+
+/**
+ * The array of objects of her steps during the journey.
+ */
+const steps = [
+    { direction: 'East', distance: 300 },
+    { direction: 'South', distance: 300 },
+    { direction: 'East', distance: 450 },
+    { direction: 'North', distance: 700 },
+    { direction: 'East', distance: 500 },
+    { direction: 'North', distance: 50 },
+    { direction: 'West', distance: 15 },
+]
+
+/**
+ * Use the above array to call the 'getFacingDirection' function using loop and log the output.
+ */
+console.log(facingDirection)
