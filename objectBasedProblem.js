287d563bf96a378d60886229b0c393515be5e950
Pragati-patel <peehupatel1998@gmail.com>
2022-11-25 17:36:17 +0530

    create object based problems and also create a problem that is how much time some specific char is come

---
+++ objectBasedProblem.js
@@ -0,0 +1,14 @@
+// input  =[
+//     {name:"Mohit",age:"20"},
+//     {name:"Raman",age:"21"},
+//     {name:"Arun",age:"20"},
+//     {name:"priya",age:"21"},
+//     {name:"peehu",age:"26"},
+//     {name:"pragati",age:"26"},
+//     ]
+
+// output ={
+//     20:[{name:"Mohit",age:"20"},
+//     {name:"Arun",age:"20"}],
+//     21:[{name:"Raman",age:"21"}]
+// }
