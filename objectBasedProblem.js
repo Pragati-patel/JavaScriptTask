d8836d7ba1d5a2e03d53f9eef353b04814c6176d
Pragati-patel <peehupatel1998@gmail.com>
2022-11-27 00:12:10 +0530

    solve object based problem

--- objectBasedProblem.js
+++ objectBasedProblem.js
@@ -12,3 +12,21 @@
 //     {name:"Arun",age:"20"}],
 //     21:[{name:"Raman",age:"21"}]
 // }
+
+
+const input = [
+    { name: "Mohit", age: "20" },
+    { name: "Raman", age: "21" },
+    { name: "Arun", age: "20" },
+    { name: "priya", age: "21" },
+    { name: "peehu", age: "26" },
+    { name: "pragati", age: "26" },
+]
+const res = {}
+input.map((item) => {
+    if (res)
+        if (res.age !== item.age) {
+            res[item.age] = input.filter((i) => i.age === item.age)
+        }
+})
+console.log(res)
