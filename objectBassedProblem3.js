50555fc3d4699a1ec0d924923945970526748647
Pragati-patel <peehupatel1998@gmail.com>
2022-11-29 17:21:24 +0530

    add two string based problem and one object based problem

---
+++ objectBassedProblem3.js
@@ -0,0 +1,9 @@
+// input = ['ram1','shyam1','mani','vineth','bajrang','ram2','shyam2','mani2','veth','bang','ram3','dhyam','aani','cineth','bajrang2'];
+// output= { r: [ 'ram1', 'ram2', 'ram3' ],
+// s: [ 'shyam1', 'shyam2' ],
+// m: [ 'mani', 'mani2' ],
+// v: [ 'vineth', 'veth' ],
+// b: [ 'bajrang', 'bang', 'bajrang2' ],
+// d: [ 'dhyam' ],
+// a: [ 'aani' ],
+// c: [ 'cineth' ] }
