e8d06cef2aa709314b26b15396a28150d938b79a
Pragati-patel <peehupatel1998@gmail.com>
2022-11-29 22:58:30 +0530

    replace blank space with the undersore in a string

--- replaceSpaceWithUnderscore.js
+++ replaceSpaceWithUnderscore.js
@@ -1,2 +1,11 @@
-let x = "eov ejn  v  j " 
+let input = "eov ejn    v  j " 
 // output = eov_ejn_v_j
+const res=[]
+input.trim().split('').map((item,index)=>{
+if(item===' '){
+    if(input[index]!==input[index+1])
+      res.push("_")
+}else res.push(item)
+})
+
+console.log(res.join(''))
