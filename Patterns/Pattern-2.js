c5fcdf63ea0a0e7e31fa2be57bf0111b6ed11f23
Pragati-patel <peehupatel1998@gmail.com>
2022-12-07 00:10:15 +0530

    add four patterns with solution

--- Patterns/Pattern-2.js
+++ Patterns/Pattern-2.js
@@ -6,26 +6,26 @@
 // *****
 
 // with  two loops
-let res = ''
-for (let i = 1; i <= 5; i++) {
-    for (let j = 1; j <= i; j++) {
+// let res = ''
+// for (let i = 1; i <= 5; i++) {
+//     for (let j = 1; j <= i; j++) {
 
-        res = res + '*'
-    }
-    res = res + '\n'
-}
-console.log(res)
+//         res = res + '*'
+//     }
+//     res = res + '\n'
+// }
+// console.log(res)
 
 
 
 
 
 // with one loop
-// let res = ''
-// for (let i = 1; i <= 5; i++) {
-//         res = res + "*"
-//     console.log(res)
-// }
+let res = ''
+for (let i = 1; i <= 5; i++) {
+        res = res + "*"
+    console.log(res)
+}
 
 
 
