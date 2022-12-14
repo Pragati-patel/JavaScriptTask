514f009222b7e2db9d33e65c2a469c4620d3e654
Pragati-patel <peehupatel1998@gmail.com>
2022-12-15 00:47:01 +0530

    add letter T and I

--- Patterns/MyPattern.js
+++ Patterns/MyPattern.js
@@ -1,54 +1,54 @@
-// let p=''
-// for(let i=1;i<=6;i++){
-//     p=p+'*'
-//     if(i<=3){
-//         for(let j=1;j<=4;j++){
-//             if((i===1 || i==3) && (j==4))p=p+' '
-//             else if(i===2 && j!==4) p=p+' '
-//             else p=p+'*'
-//         }
-//     }
-//     p=p+'\n'
-// }
+let p=''
+for(let i=1;i<=6;i++){
+    p=p+'*'
+    if(i<=3){
+        for(let j=1;j<=4;j++){
+            if((i===1 || i==3) && (j==4))p=p+' '
+            else if(i===2 && j!==4) p=p+' '
+            else p=p+'*'
+        }
+    }
+    p=p+'\n'
+}
 // console.log(p)
 
-// let R = ''
-// let temp = 0
-// for (let i = 1; i <= 6; i++) {
-//     R = R + '*'
-//     if (i <= 3) {
-//         for (let j = 1; j <= 4; j++) {
-//             if ((i === 1 || i == 3) && (j == 4)) R = R + ' '
-//             else if (i === 2 && j !== 4) R = R + ' '
-//             else R = R + '*'
-//         }
-//     } else{        
-//         for (let z = 0; z <= temp; z++) {
-//             if(z===temp)R=R+'*'
-//            else R=R+' '
-//         }
+let R = ''
+let temp = 0
+for (let i = 1; i <= 6; i++) {
+    R = R + '*'
+    if (i <= 3) {
+        for (let j = 1; j <= 4; j++) {
+            if ((i === 1 || i == 3) && (j == 4)) R = R + ' '
+            else if (i === 2 && j !== 4) R = R + ' '
+            else R = R + '*'
+        }
+    } else{        
+        for (let z = 0; z <= temp; z++) {
+            if(z===temp)R=R+'*'
+           else R=R+' '
+        }
 
-//     }
-//     temp += 1
-//     R = R + '\n'
-// }
+    }
+    temp += 1
+    R = R + '\n'
+}
 // console.log(R)
 
-// let A=''
-// let x=1
-// for(let i=0;i<=4;i++){
-//     for(let j=1;j<5-i;j++){
-//         A=A+' '
-//     }
-//     A=A+'*'
-//     for(let j=1;j<x;j++){
-//         if(i==2)A=A+'*'
-//         else A=A+' '
-//     }
-//     x+=2
-//     if(i>0)A=A+'*'
-//     A=A+'\n'
-// }
+let A=''
+let x=1
+for(let i=0;i<=4;i++){
+    for(let j=1;j<5-i;j++){
+        A=A+' '
+    }
+    A=A+'*'
+    for(let j=1;j<x;j++){
+        if(i==2)A=A+'*'
+        else A=A+' '
+    }
+    x+=2
+    if(i>0)A=A+'*'
+    A=A+'\n'
+}
 // console.log(A)
 
 let G = ''
@@ -61,4 +61,27 @@ for (let i = 0; i <= 4; i++) {
     else control=control-1
     G=G+'*'+'\n'
 }
-console.log(G)
+// console.log(G)
+
+
+
+
+let T=''
+for(let i=1; i<=6 ; i++){
+    for(let j=1;j<=6;j++){
+        if(i!==1 && j!=3) T=T+' '  
+        else T=T+'*'  
+    }
+    T=T+'\n'
+}
+// console.log(T)
+
+let I=''
+for(let i=1; i<=6 ; i++){
+    for(let j=1;j<=6;j++){
+        if(i!==1 && j!=3 && i!==6) I=I+' '  
+        else I=I+'*'  
+    }
+    I=I+'\n'
+}
+console.log(I)
